73220f434d6e9aabccb7a9c2cd1631e1f75fd165
ferrah anissa <anissaferrah24@gmail.com>
2025-05-04 17:51:50 +0200

    update partie1

--- script.sql
+++ script.sql
@@ -2,9 +2,10 @@
 ALTER PLUGGABLE DATABASE orclpdb OPEN;
 connect sys@orclpdb as sysdba
 
---creation des tablespaces et l'utilisateur sql  
-create tablespace  SQL3_TBS datafile 'C:\Users\HERO-INFO\Desktop\M1-sii\s2\bdd\projet bdd 2024-2025\bda-project\bdd-project\tablespaces\tbs_002.dat' size 100M autoextend on;
-create temporary tablespace SQL3_TempTBS  tempfile 'C:\C:\Users\HERO-INFO\Desktop\M1-sii\s2\bdd\projet bdd 2024-2025\bda-project\bdd-project\tablespaces\temp_002.dat' size 100M autoextend on;
+--creation des tablespaces et l'utilisateur sql  se connecte à la base de données pluggable
+create tablespace  SQL3_TBS datafile 'C:\tablespaces\tbs_002.dat' size 100M autoextend on;
+create temporary tablespace SQL3_TempTBS  tempfile 'C:\tablespaces\temp_002.dat' size 100M autoextend on;
 create user  SQL3 identified by sql3 default tablespace SQL3_TBS temporary tablespace SQL3_TempTBS;
 grant all privileges to SQL3;
 connect SQL3/sql3@orclpdb;
+--creation de la table et insertion des données
